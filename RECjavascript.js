e56d2a43766e67515e8190e73d04be9de2e1b22b
carla25maelin <maelinadultafuturakim@gmail.com>
2024-06-17 01:41:30 -0400

    Add files via upload

---
+++ RECjavascript.js
@@ -0,0 +1,20 @@
+document.getElementById('box1').addEventListener('click', function() {
+    alert('Nunca abandones tus sueños: duerme 5 minutos mas.');
+});
+
+document.getElementById('box2').addEventListener('click', function() {
+    alert('Las estrellas dicen que hoy encontraras algo que perdiste... probablemente tu paciencia');
+});
+
+document.getElementById('box3').addEventListener('click', function() {
+    alert('El universo conspira para que hoy te tropieces con esa esquina de la mesa. ¡CUIDADO!');
+});
+
+document.getElementById('box4').addEventListener('click', function() {
+    alert('¡ATENCION! Hoy es un buen dia para una siesta epica. No te resistas.');
+});
+
+document.getElementById('box5').addEventListener('click', function() {
+    alert('Si lo que deseas no viene hacia ti, usa el plan ve.');
+});
+
